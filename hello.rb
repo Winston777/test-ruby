6e89ca54dd8275ba5bc4d31a30477383a0920033
Zaalem <nicolas.belleudy@ice-et.fr>
2018-09-26 11:36:06 +0200

    Last Name

--- hello.rb
+++ hello.rb
@@ -1,2 +1,10 @@
-user = gets.chomp
-print "Bonjour " + user + "!!!"
+print What's you name?
+first_name = gets.chomp
+
+print What's your last name?
+last_name = gets.chomp
+
+print "Bonjour " + first_name + last_name + "!!!"
+
+
+
