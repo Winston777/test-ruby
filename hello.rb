8dea3d1f07a0e4bebac637911f932bbef9f69359
Zaalem <nicolas.belleudy@ice-et.fr>
2018-09-26 11:38:59 +0200

    Last Name Fixed

--- hello.rb
+++ hello.rb
@@ -1,10 +1,11 @@
-print What's you name?
+
+print "What's you first name?"
 first_name = gets.chomp
 
-print What's your last name?
+print "What's your last name?"
 last_name = gets.chomp
 
-print "Bonjour " + first_name + last_name + "!!!"
+print "Bonjour " + first_name + " " + last_name + "!!!"
 
 
 
